a95ee3232565b395e99d46b63b12884b189ac7c3
IDGS-801-21000016 <u25936629@gmail.com>
2024-02-07 18:54:58 -0600

    JURH - 7/2/2024 calculo de resistencia

--- formColores/form.py
+++ formColores/form.py
@@ -1,18 +1,9 @@
 from flask_wtf import Form
-from wtforms import FloatField
-
 from wtforms import SelectField, RadioField
 
 
-class Inputs(Form):
+class InputsForm(Form):
   color1 = SelectField('color1',choices=['Negro','Cafe','Rojo','Naranja','Amarillo','Verde','Azul','Violeta','Gris','Blanco'])
   color2 = SelectField('color2',choices=['Negro','Cafe','Rojo','Naranja','Amarillo','Verde','Azul','Violeta','Gris','Blanco'])
   color3 = SelectField('color3',choices=['Negro','Cafe','Rojo','Naranja','Amarillo','Verde','Azul','Violeta','Gris','Blanco'])
   tolerancia = RadioField('Tolerancia',choices=['Oro','Plata'], default="Oro")
-
-class InputsForm(Form):
-    x1 = FloatField('x1')
-    x2 = FloatField('x2')
-    y1 = FloatField('y1')
-    y2 = FloatField('y2')
-    
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import math
-from formsDistancia.form import InputsForm
+from formsDistancia.form import InputsForm, Inputs as Ip
 
 app = Flask(__name__)
 
@@ -8,6 +8,92 @@ app = Flask(__name__)
 @app.route('/')
 def operasBas():
   	return render_template('operasBas.html')
+
+@app.route('/resistencia',  methods=['GET', 'POST'])
+def resistencia():
+    form = Ip(request.form)
+    color1 = None
+    color2 = None
+    color3 = None
+    tolerancia = None
+    isRegistered = {
+        color1:0
+    }
+    #['Negro','Cafe','Rojo','Naranja','Amarillo','Verde','Azul','Violeta','Gris','Blanco']
+    tableRegistecia = {
+        'Negro':'0',
+        'Cafe': '1',
+        'Rojo':'2',
+        'Naranja':'3',
+        'Amarillo':'4',
+        'Verde':'5',
+        'Azul':'6',
+        'Violeta':'7',
+        'Gris':'8',
+        'Blanco':'9'
+    }
+
+    tole = {
+        'Oro':5,
+        'Plata':10
+    }
+
+    bandaMulti ={
+        'Negro':1,
+        'Cafe': 10,
+        'Rojo':100,
+        'Naranja':1000,
+        'Amarillo':10000,
+        'Verde':100000,
+        'Azul':1000000,
+        'Violeta':10000000,
+        'Gris':100000000,
+        'Blanco':1000000000
+    }
+
+    color={
+        'Negro':'style:"background-color:black;"',
+        'Cafe': 'style:"background-color:brown;"',
+        'Rojo':'style:"background-color:red;"',
+        'Naranja':'style:"background-color:orange;"',
+        'Amarillo':'style:"background-color:yellow;"',
+        'Verde':'style:"background-color:green;"',
+        'Azul':'style:"background-color:blue;"',
+        'Violeta':'style:"background-color:violet;"',
+        'Gris':'style:"background-color:gray;"',
+        'Blanco':'style:"background-color:white;"'
+    }
+
+    if request.method == 'POST':
+        color1 = request.form['color1']
+        color2 = request.form['color2']
+        color3 = request.form['color3']
+        tolerancia = request.form['tolerancia']
+
+        c1 =color[color1]
+        c2= color[color2]
+        c3 = color[color3]
+        
+        v1 = int(tableRegistecia[color1])
+        v2 = int(tableRegistecia[color2])
+        band = int(bandaMulti[color3])
+        tolerancia = tole[tolerancia]
+
+        valor =int( f"{v1}{v2}")* band
+
+        if tolerancia == 5:
+            tole = 'Dorado 5%'
+        else:
+            tole = 'Plateado 10%'
+                
+        valorMinimo = valor - (valor * tolerancia/100)
+        valorMaximo = valor + (valor * tolerancia/100)
+
+
+        return render_template('registencia.html', form=form, color1=color1,color2=color2, color3=color3, valor=valor, valorMaximo=valorMaximo, valorMinimo=valorMinimo,tole=tole,c1=c1,c2=c2,c3=c3)
+
+    return render_template('registencia.html', form=form)
+
 
 @app.route('/distancia', methods=['GET', 'POST'])
 def distancia():
