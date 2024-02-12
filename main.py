f845c687ce6922b19b04dbee30e98b14180caf30
IDGS-801-21000016 <u25936629@gmail.com>
2024-02-12 17:23:24 -0600

    JURH - 7/2/2024 calculo de resistencia

--- main.py
+++ main.py
@@ -73,6 +73,8 @@ def resistencia():
         c1 =color[color1]
         c2= color[color2]
         c3 = color[color3]
+
+        print(type(c1))
         
         v1 = int(tableRegistecia[color1])
         v2 = int(tableRegistecia[color2])
@@ -132,6 +134,74 @@ def resultado():
             return f"La division de {n1} y {n2} es {resultado}"
         else:
             return "Error"
+        
+
+@dataclass
+class GeneratePyramid:
+  n: int
+
+  def imprimeEstrellas(self, n):
+    for i in range(self.n):
+      for j in range(i+1):
+        print("*", end="")
+      print("")
+
+def pyramid():
+    n = input("Ingrese el numero de filas: ")
+    obj = GeneratePyramid(int(n))
+    obj.imprimeEstrellas(5)
+
+
+from dataclasses import dataclass
+
+@dataclass
+class countNumbers:
+  listSize: int
+
+  def sortList(self, list):
+    for i in range(len(list)):
+      for j in range(len(list)-1):
+        if list[j] > list[j+1]:
+          aux = list[j]
+          list[j] = list[j+1]
+          list[j+1] = aux
+    return list
+
+  def is_Even(self, number):
+    if number % 2 == 0:
+      return True
+    else:
+      return False
+
+  def print_is_Even(self, list):
+    even=[]
+    odd=[]
+    for i in range(len(list)):
+      if self.is_Even(list[i]):
+        even.append(list[i])
+      else:
+        odd.append(list[i])
+    return even, odd
+
+  def showRepiteNumbers(self, list):
+    for i in range(len(list)):
+      if list.count(list[i]) > 1:
+        print(list[i], "se repite", list.count(list[i]), "veces")
+
+
+def countLista():
+  listSize = int(input("Ingrese el tamaño de la lista: "))
+  obj = countNumbers(listSize)
+  list = []
+  for i in range(listSize):
+    list.append(int(input("Ingrese el numero: ")))
+  print(f"La lista es original: {list}")
+  orderedList = obj.sortList(list)
+  print(f"La lista ordenada es: {orderedList}")
+  even, odd = obj.print_is_Even(orderedList)
+  print(f"Los numeros pares son: {even}")
+  print(f"Los numeros impares son: {odd}")
+  obj.showRepiteNumbers(orderedList)
       
 if __name__ == '__main__':
     app.run(debug=True)
